7b358fdc4f3c2c7f780c3bd84f80ae9b082c9c7d
Ryan Good <ryan.good@zetier.com>
2025-06-03 17:53:02 -0500

    disable eslint

--- next.config.ts
+++ next.config.ts
@@ -4,4 +4,11 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+// Get rid of this when we know what we are doing.
+module.exports = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  }
+}
+
 export default nextConfig;
